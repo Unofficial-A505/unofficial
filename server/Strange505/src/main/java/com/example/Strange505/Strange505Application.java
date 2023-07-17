5db5fc50bf26aa6cc86d1e5a07dc5b79798d5a90
정승구 <jsggo2001@naver.com>
2023-07-17 16:45:39 +0900

    feat: intial project

---
+++ server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
@@ -0,0 +1,13 @@
+package com.example.Strange505;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Strange505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Strange505Application.class, args);
+	}
+
+}
---
+++ server/Strange505/src/test/java/com/example/Strange505/Strange505ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Strange505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Strange505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
