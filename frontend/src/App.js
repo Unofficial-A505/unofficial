d032efb3674df4d80dcad0d8ff7d9adb96c2bf8a
Sseo Jjam <sjh960815@gmail.com>
2023-07-18 00:36:07 +0900

    feat: 로그인&회원가입 페이지 중간 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Signup from './pages/SignupPage/Signup.js'
+import Login from './components/Login/Login.js'
+
+import { useState } from 'react';
+import Signup2 from './pages/SignupPage/Signup2';
 
 function App() {
+
+  // 모달창 노출 여부 state
+  const [modalOpen, setModalOpen] = useState(false);
+  // 모달창 노출
+  const showModal = () => {
+      setModalOpen(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Signup />
+      <div>
+        <button onClick={showModal}>모달 띄우기</button>
+        {modalOpen && <Login setModalOpen={setModalOpen} />}
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/SignupPage/Signup3.js
@@ -0,0 +1,6 @@
+import styles from './Signup.module.css';
+
+export default function Signup2(){
+  return(
+    <div>안녕 여긴 회원가입3 페이지야.</div>
+)}
