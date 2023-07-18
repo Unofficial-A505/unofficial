89175076b8959178c9e7eaba48f83825ed56f3ef
aprilzu0427@gmail.com <aprilzu0427@gmail.co>
2023-07-18 11:02:30 +0900

    Refactor : add createBowserRouter

---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,4 @@
+export default function Root(){
+  return(
+    <div>Root page</div>
+)}
--- frontend/src/pages/SignupPage/Signup.js
+++ frontend/src/pages/SignupPage/Signup.js
@@ -1,15 +1,48 @@
+// import { Routes, Route } from 'react-router-dom'
+// import Signup1 from './Signup1'
+// import Signup2 from './Signup2'
+// import Signup3 from './Signup3'
+
+
+// export default function Signup(){
+//   return (
+//     <Routes>
+//       <Route path='/signup' element={ <Signup1 /> } />
+//       <Route path='/register' element={ <Signup2 /> } />
+//       <Route path='/conplete' element={ <Signup3 /> } />
+//     </Routes>
+//   )
+// }
+import React from 'react';
+// import logo from './logo.svg';
+// import '../App.css';
 import { Routes, Route } from 'react-router-dom'
-import Signup1 from './Signup1'
-import Signup2 from './Signup2'
-import Signup3 from './Signup3'
+// import Signup from './pages/SignupPage/Signup.js'
+import Login from '@/src/components/Login/Login.js'
 
+import { useState } from 'react';
+import Signup2 from './SignupPage/Signup2';
 
-export default function Signup(){
+import { Outlet } from 'react-router-dom';
+
+function Signup() {
+
+  // 모달창 노출 여부 state
+  const [modalOpen, setModalOpen] = useState(false);
+  // 모달창 노출
+  const showModal = () => {
+      setModalOpen(true);
+  }
+
   return (
-    <Routes>
-      <Route path='/signup' element={ <Signup1 /> } />
-      <Route path='/register' element={ <Signup2 /> } />
-      <Route path='/conplete' element={ <Signup3 /> } />
-    </Routes>
+    <div className="App">
+      <div>
+        <button onClick={showModal}>모달 띄우기</button>
+        {modalOpen && <Login setModalOpen={setModalOpen} />}
+      </div>
+      <Outlet />
+    </div>
   )
 }
+
+export default Signup;
