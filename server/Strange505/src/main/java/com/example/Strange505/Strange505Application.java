9bc6e94ccefbea59f498e411700e2666efd5939a
고동우 <shp05179@gmail.com>
2023-07-18 10:38:56 +0900

    feat: 크론 스케쥴러 애노테이션 추가

--- server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
+++ server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
@@ -2,8 +2,10 @@ package com.example.Strange505;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Strange505Application {
 
 	public static void main(String[] args) {
