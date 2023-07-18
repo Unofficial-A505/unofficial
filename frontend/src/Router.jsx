091e6c601e96a7793180ff376007579160d77098
aprilzu0427@gmail.com <aprilzu0427@gmail.co>
2023-07-18 14:20:21 +0900

    feat: add MainPage.jsx and MainPage.module.css
    fix route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
 import Signup from './pages/SignupPage/Signup'
 import Signup1 from './pages/SignupPage/Signup1'
 import Signup2 from './pages/SignupPage/Signup2'
 import Signup3 from './pages/SignupPage/Signup3'
+
+import App from './App';
+import MainPage from './pages/MainPage/MainPage'
 
 const router = createBrowserRouter([
   {
@@ -24,7 +26,13 @@ const router = createBrowserRouter([
   },
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    children: [
+      { 
+        path: '',
+        element: <MainPage />
+      }
+    ]
   }
 ])
 
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,32 @@
+import styles from './MainPage.module.css'
+
+export default function MainPage(){
+  return (
+    <section>
+      <div>
+        carousel
+      </div>
+      <div>
+        <div>
+          <p>SSABRY Time에서 자유롭게 소통해보세요</p>
+          <div>
+            <button>로그인</button>
+            <button>회원가입</button>
+          </div>
+        </div>
+        <div>
+          <button>
+            <img src="" alt="" />
+            <p>교육지원금 서명 생성기</p>
+            <img src="" alt="" />
+          </button>
+          <button>
+            <img src="" alt="" />
+            <p>삼성 청년 SW 아카데미</p>
+          </button> 
+          <button>SW Expert Academy</button>
+        </div>
+      </div>
+    </section>
+  )
+}
