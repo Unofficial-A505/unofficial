8a0570b7c6d5ebbfe26246b6885010cf2889ddbd
aprilzu0427@gmail.com <aprilzu0427@gmail.co>
2023-07-18 15:52:35 +0900

    feat: add SearchAll component

---
+++ frontend/src/components/SearchAll/SearchAll.jsx
@@ -0,0 +1,9 @@
+import styles from './SearchAll.module.css'
+
+export default function SearchAll(){
+  return(
+    <div>
+      <input type="text" placeholder="제목과 내용의 키워드를 검색해보세요!" />
+    </div>
+  );
+}
