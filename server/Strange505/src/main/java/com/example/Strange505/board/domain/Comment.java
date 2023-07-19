03d9d0a062c5dce108c958c936f2bd83d44f0a95
한다솜 <hmdasom@naver.com>
2023-07-19 13:24:55 +0900

    feat: 댓글 엔티티 생성

--- server/Strange505/src/main/java/com/example/Strange505/board/domain/Comment.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/domain/Comment.java
@@ -1,4 +1,33 @@
 package com.example.Strange505.board.domain;
 
+import com.example.Strange505.user.domain.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+    @Column(length = 1000)
+    private String Content;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> children;
+    private LocalDateTime createTime;
+    private LocalDateTime modifyTime;
+
 }
