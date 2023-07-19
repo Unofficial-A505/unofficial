de4cc3c0b8c0ebd632d477529a3831609e1ac097
한다솜 <hmdasom@naver.com>
2023-07-19 13:13:22 +0900

    feat: 유저 엔티티 추가

---
+++ server/Strange505/src/main/java/com/example/Strange505/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.example.Strange505.user.domain;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String encryptedPwd;
+    @Column(nullable = false, unique = true)
+    private String local;
+    @Column(nullable = false, unique = true)
+    private int gen;
+    private int point;
+    private boolean is_activated;
+    private boolean is_auth;
+    private boolean is_withdraw;
+}
