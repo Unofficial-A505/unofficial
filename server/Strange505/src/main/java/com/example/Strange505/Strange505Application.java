c403ce6820bf1ab6a9cc381489cff00e9317d1b1
한다솜 <hmdasom@naver.com>
2023-07-19 11:16:51 +0900

    feat: 게시글 엔티티 구현

--- server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
+++ server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
@@ -2,8 +2,9 @@ package com.example.Strange505;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Strange505Application {
 
 	public static void main(String[] args) {
--- server/Strange505/src/main/java/com/example/Strange505/board/domain/Article.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/domain/Article.java
@@ -1,4 +1,22 @@
 package com.example.Strange505.board.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Article {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+    private Long likes;
+    private Long views;
+    private LocalDateTime createTime;
+    private LocalDateTime modifyTime;
 }
