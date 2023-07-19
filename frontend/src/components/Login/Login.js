c4e1e96be74cbf22f7221e33463f85e55559bc09
서재현 <sjh960815@naver.com>
2023-07-19 13:52:20 +0900

    feat: 로그인 서버요청 구현

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 import logo from './../../assets/images/whale.png'
+import requestLogin from './LoginFunc.js'
 
 export default function Login({ setModalOpen }){
   // 모달 끄기 
   const closeModal = ()=>{
     setModalOpen(false);
   };
-
+  // 로그인 정보
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const onEmailHandler = (event)=>{
+    setEmail(event.target.value)
+  }
+  const onPasswordHandler = (event)=>{
+    setPassword(event.target.value)
+  }
+  const login = ()=>{
+    if (!email || !password) {
+      alert('이메일 또는 비밀번호를 입력하세요.')
+      return
+    } requestLogin(email, password)
+  }
+  
   return (
     <>
       <div className={styles.overlay} onClick={closeModal}></div>
@@ -20,10 +37,10 @@ export default function Login({ setModalOpen }){
         </div>
         <form>
           <div>
-            <input type="text" className="form-control mb-1" placeholder="이메일" />
-            <input type="text" className="form-control" placeholder="비밀번호" />
+            <input type="email" className="form-control mb-1" placeholder="이메일" onChange={onEmailHandler} />
+            <input type="password" className="form-control" placeholder="비밀번호" onChange={onPasswordHandler} />
           </div>
-          <input className='mt-3' type="submit" value="로그인" />
+          <input className='mt-3' type="submit" value="로그인" onClick={login} />
         </form>
         <div className='mt-3' style={{ margin:'0 auto' }}>
           <span>싸브리타임에 처음이신가요?&nbsp;&nbsp;</span>
