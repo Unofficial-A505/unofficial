30fc005aa77b17dd53e66c53a614022899a4ab4a
한다솜 <hmdasom@naver.com>
2023-07-19 13:58:30 +0900

    feat: 게시글 DTO 생성

---
+++ server/Strange505/src/main/java/com/example/Strange505/board/dto/ArticleDTO.java
@@ -0,0 +1,25 @@
+package com.example.Strange505.board.dto;
+
+import com.example.Strange505.board.domain.Board;
+import com.example.Strange505.user.domain.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ArticleDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private User user;
+    private Board board;
+    private Long likes;
+    private Long views;
+    private LocalDateTime createTime;
+    private LocalDateTime modifyTime;
+
+}
