67f68b92689721ed7e7ada74dd1127f999d1eb4b
한다솜 <hmdasom@naver.com>
2023-07-19 11:07:53 +0900

    feat: 게시판 엔티티 구현

--- server/Strange505/src/main/java/com/example/Strange505/board/domain/Board.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/domain/Board.java
@@ -1,4 +1,23 @@
 package com.example.Strange505.board.domain;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Boards")
+@Getter
+@Setter
 public class Board {
+    @Id @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+    private String name;
+    private LocalDateTime createTime;
+    private LocalDateTime modifyTime;
+    @OneToMany(mappedBy = "board")
+    private List<Article> articles = new ArrayList<>();
 }
