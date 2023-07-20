ed68d7a57a7138adc39e85fe8a7aaf2b19adda91
서재현 <sjh960815@naver.com>
2023-07-20 17:25:51 +0900

    feat: 메인페이지 식단 캐러셀 생성 시작

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -12,6 +12,8 @@ import BoardsAll from './pages/BoardsAll/BoardsAll'
 import PostDetail from './pages/PostDetail/PostDetail'
 import CreatePostPage from './pages/CreatePostPage/CreatePostPage'
 
+import Carousel from './components/Carousel/Carousel'
+
 const router = createBrowserRouter([
   {
     path: '/signup',
@@ -36,6 +38,9 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: '', element: <MainPage />},
+      {
+        path: 'meal', element: <Carousel />
+      },
       { 
         // path: ':userId', 
         path: 'user', element: <MyPage />},
