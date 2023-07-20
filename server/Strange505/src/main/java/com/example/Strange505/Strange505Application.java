93cb6f2b1e56a411ce34ea0f463eafff05bbb7e5
kanghee96 <kanghee96@gmail.com>
2023-07-21 08:27:52 +0900

    fix: 505어플리케이션 수정

--- server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
+++ server/Strange505/src/main/java/com/example/Strange505/Strange505Application.java
@@ -2,12 +2,15 @@ package com.example.Strange505;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Strange505Application {
-
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,";
 	public static void main(String[] args) {
 		SpringApplication.run(Strange505Application.class, args);
+
 	}
 
 }
