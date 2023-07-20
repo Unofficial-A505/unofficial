0360d9f7ff05db13adab05a8d9ae6d49c9bf255c
고동우 <shp05179@gmail.com>
2023-07-20 11:07:54 +0900

    refactor: 점심메뉴 service 인터페이스화

--- server/Strange505/src/main/java/com/example/Strange505/lunch/service/LunchService.java
+++ server/Strange505/src/main/java/com/example/Strange505/lunch/service/LunchService.java
@@ -1,44 +1,17 @@
 package com.example.Strange505.lunch.service;
 
 import com.example.Strange505.lunch.Lunch;
-import com.example.Strange505.lunch.cron.LunchScrapCron;
-import com.example.Strange505.lunch.repository.LunchRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class LunchService {
-    LunchRepository lunchRepository;
-    LunchScrapCron lunchScrapCron;
+public interface LunchService {
+    List<Lunch> getTodayLunch(String date);
 
-    public LunchService(LunchRepository lunchRepository, LunchScrapCron lunchScrapCron) {
-        this.lunchRepository = lunchRepository;
-        this.lunchScrapCron = lunchScrapCron;
-    }
+    List<Lunch> getLunches();
 
-    public List<Lunch> getTodayLunch(String date) {
-        return lunchRepository.findByDate(date);
-    }
-
-    public List<Lunch> getLunches() {
-        return lunchRepository.findAll();
-    }
-
-    public boolean forceCron() throws Exception {
-        lunchScrapCron.cronJob();
-        return true;
-    }
+    boolean forceCron() throws Exception;
 
     @Transactional
-    public boolean like(long id) {
-        Lunch lunch = lunchRepository.findById(id).get();
-        if (lunch != null) {
-            lunch.setLikes(lunch.getLikes() + 1);
-            lunchRepository.save(lunch);
-            return true;
-        }
-        return false;
-    }
+    boolean like(long id);
 }
