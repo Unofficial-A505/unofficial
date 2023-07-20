425410c8803a8e34e2b9e167f9b8556663ddd67c
한다솜 <hmdasom@naver.com>
2023-07-20 17:44:02 +0900

    feat: 게시글을 게시판에 추가하는 메서드 추가

--- server/Strange505/src/main/java/com/example/Strange505/board/domain/Article.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/domain/Article.java
@@ -2,6 +2,7 @@ package com.example.Strange505.board.domain;
 
 import com.example.Strange505.user.domain.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -9,17 +10,22 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Table(name = "Articles")
+@Builder
 public class Article {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "article_id")
     private Long id;
     private String title;
+
     @Column(length = 5000)
     private String content;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
@@ -27,4 +33,10 @@ public class Article {
     private Long views;
     private LocalDateTime createTime;
     private LocalDateTime modifyTime;
+
+    // 게시판에 게시글 추가
+    public void addToBoard(Board board) {
+        this.board = board;
+        this.board.getArticles().add(this);
+    }
 }
--- server/Strange505/src/main/java/com/example/Strange505/board/service/ArticleService.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/service/ArticleService.java
@@ -6,13 +6,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ArticleService {
-//    void createArticle(ArticleDTO articleDTO);
-//    ArticleDTO getArticleById(Long id);
-//    List<ArticleDTO> getAllArticles();
-//    List<ArticleDTO> getArticlesByTitle(String title);
-//    List<ArticleDTO> getArticlesByContent(String content);
-//    void updateArticle(Long id, ArticleDTO articleDTO);
-//    void deleteArticle(Long id);
+public interface ArticleService {
+    void createArticle(ArticleDTO articleDTO);
+
+    ArticleDTO getArticleById(Long id);
+
+    List<ArticleDTO> getAllArticles();
+
+    List<ArticleDTO> getArticlesByTitle(String title);
+
+    List<ArticleDTO> getArticlesByContent(String content);
+
+    void updateArticle(Long id, ArticleDTO articleDTO);
+
+    void deleteArticle(Long id);
 }
