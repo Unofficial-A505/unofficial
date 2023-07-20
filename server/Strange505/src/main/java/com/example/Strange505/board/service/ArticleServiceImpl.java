c2d18135e108d024da03ef2ef50d8fcca3ea4ead
Amyhds <dasomhan524@gmail.com>
2023-07-21 01:00:07 +0900

    feat: 게시글 서비스 구현 중 커스텀 레포지토리 구상 중

--- server/Strange505/src/main/java/com/example/Strange505/board/service/ArticleServiceImpl.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/service/ArticleServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.Strange505.board.service;
 import com.example.Strange505.board.domain.Article;
 import com.example.Strange505.board.domain.Board;
 import com.example.Strange505.board.dto.ArticleDTO;
+import com.example.Strange505.board.dto.BoardDTO;
 import com.example.Strange505.board.repository.ArticleRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,43 +17,55 @@ import java.util.List;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ArticleServiceImpl implements ArticleService{
+public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleRepository articleRepository;
 
     @Override
     public void createArticle(ArticleDTO articleDTO) {
-//        Board findBoard = findBoardBy
-//        Article article = Article.builder().
+        Article article = Article.builder()
+                .id(articleDTO.getId())
+                .title(articleDTO.getTitle())
+                .content(articleDTO.getContent())
+                .user(articleDTO.getUser())
+                .board(articleDTO.getBoard())
+                .createTime(articleDTO.getCreateTime())
+                .modifyTime(articleDTO.getModifyTime())
+                .likes(articleDTO.getLikes())
+                .views(articleDTO.getViews())
+                .build();
+        articleRepository.save(article);
+        article.addToBoard(articleDTO.getBoard());
     }
 
     @Override
-    public ArticleDTO getArticleById(Long id) {
+    public Article getArticleById(Long id) {
+        return articleRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<Article> getArticlesByBoard(String board) {
+        return articleRepository.findByBoard(board).orElseThrow();
+    }
+
+    @Override
+    public List<Article> getArticlesByTitle(String title) {
         return null;
     }
 
     @Override
-    public List<ArticleDTO> getAllArticles() {
-        return null;
-    }
-
-    @Override
-    public List<ArticleDTO> getArticlesByTitle(String title) {
-        return null;
-    }
-
-    @Override
-    public List<ArticleDTO> getArticlesByContent(String content) {
+    public List<Article> getArticlesByContent(String content) {
         return null;
     }
 
     @Override
     public void updateArticle(Long id, ArticleDTO articleDTO) {
-
+        Article article = articleRepository.findById(id).orElseThrow(() -> new RuntimeException("Article not found"));
+//        article.update();
     }
 
     @Override
     public void deleteArticle(Long id) {
-
+        articleRepository.deleteById(id);
     }
 }
