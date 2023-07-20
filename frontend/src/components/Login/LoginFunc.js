2dd4557b1c308956a9ff274d11aa9bd5e69638bf
서재현 <sjh960815@naver.com>
2023-07-20 17:26:36 +0900

    fix: JWT 토큰 저장 알고리즘 수정

--- frontend/src/components/Login/LoginFunc.js
+++ frontend/src/components/Login/LoginFunc.js
@@ -1,35 +1,37 @@
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
+import { useState } from 'react'
 import { setAccessToken } from '../../store/loginSlice'
 import { useNavigate } from 'react-router-dom'
 
 const serverURL = 'http://70.12.247.35:8080'
 
-const requestLogin = async (email, password)=>
-  await axios.post(
-    `${serverURL}/api/auth/login`,
-    {
-      email: email,
-      password: password,
-    },
-    // 헤더에 Authorization 항목이 있는 요청
-    // { withCredentials: true }
-  )
-  .then((res)=>{
-    alert('성공')
-    // console.log(res.headers)
-    // token이 필요한 API 요청 시 header Authorization에 token 담아서 보내기
-    // axios.defaults.headers.common[
-    //   "Authorization"
-    // ] = `Bearer ${res.data.access_token}`
-    useDispatch(setAccessToken(`${res.headers.authorization}`))
-    localStorage.setItem('refresh_token', `${res.headers.refresh_token}`)
-    // useNavigate('/')
-  })
-  .catch((err) => {
-    console.log(err)
-    alert("실패: 이메일 혹은 비밀번호를 확인하세요.")
-  })
+export default function useRequestLogin(email, password){
+
+  const dispatch = useDispatch();
+
+  const requestLogin = async (email, password) => {
+  
+    try {
+      const response = await axios.post(`${serverURL}/api/auth/login`, {
+        email: email,
+        password: password,
+      });
+  
+      console.log(response.headers);
+  
+      const accessToken = response.headers.authorization;
+      dispatch(setAccessToken(accessToken));
+  
+      localStorage.setItem('refresh_token', response.headers.refresh_token);
+      alert('성공');
+      // useNavigate('/');
+    } catch (err) {
+      console.log(err);
+      alert('실패: 이메일 혹은 비밀번호를 확인하세요.');
+    }
+  };
+}
 
 // access_token 요청
 export const requestAccessToken = async (refresh_token) => {
@@ -55,5 +57,3 @@ export const checkAccessToken = async (refresh_token) => {
     return axios.defaults.headers.common["Authorization"].split(" ")[1];
   }
 };
-
-export default requestLogin
