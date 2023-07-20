c6e30f6b7638ecd2d9278b0749ca14e10ec6e89e
Amyhds <dasomhan524@gmail.com>
2023-07-21 00:57:41 +0900

    feat: 게시판 이름으로 게시글 찾는 메서드 추가

--- server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepository.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepository.java
@@ -1,7 +1,12 @@
 package com.example.Strange505.board.repository;
 
 import com.example.Strange505.board.domain.Article;
+import com.example.Strange505.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Optional<List<Article>> findByBoard(String board);
 }
