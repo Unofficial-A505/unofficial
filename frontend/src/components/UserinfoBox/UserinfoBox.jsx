b115712998dce4ce59aa368dfb3cb6f179c4420b
aprilzu0427@gmail.com <aprilzu0427@gmail.co>
2023-07-21 10:35:46 +0900

    style: userinfobox login전 view 주석 해제

--- frontend/src/components/UserinfoBox/UserinfoBox.jsx
+++ frontend/src/components/UserinfoBox/UserinfoBox.jsx
@@ -15,40 +15,40 @@ export default function UserinfoBox(){
 
   const navigate = useNavigate();
   // user정보 없는 상황
-  // return(
-  //   <div className={styles.usercontainer}>
-  //     <div className={styles.logincenterbox}>
-  //       <p className={styles.unloginmessage}>SSABRY Time에서 자유롭게 소통해보세요</p>
-  //       <div className={styles.loginContainer}>
-  //         <button className={styles.userButton}>로그인</button>
-  //         <button className={styles.userButton}>회원가입</button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
-  // user정보 있는 상황
   return(
     <div className={styles.usercontainer}>
-      <div className={styles.usercenterbox}>
-        <div className={styles.usertopContainer}>
-          <p className={styles.hellomessage}>안녕하세요!</p>
-          <button className={styles.mypageButton} onClick={() => navigate('/user')}>마이페이지<IoIosArrowForward size="15"/></button>
-        </div>
-
-        <div className={styles.usermidContainer}>
-          <button class={styles.secondmypageButton} onClick={() => navigate('/user')}>{user}</button><span class={styles.secondhelloMessage}>의 이야기를 들려주세요</span>
-          <li class={styles.adverMessage}>진행중인 <button class={styles.adverButton} onClick={() => navigate('/user/advertisement')}>광고</button>가 없습니다.</li>
-        </div> 
-        
-        <div class={styles.mypageContent}>
-          <button class={styles.mymileButton} onClick={() => navigate('/user/advertisement')}><RiDatabase2Line class={styles.mymileIcon} /><p class={styles.mileageTotal}>{mileage}</p></button>
-          <div class={styles.mypostsAndcomments}>
-            <button class={styles.myButton} onClick={() => navigate('/user/activity')}><BsFileEarmarkText class={styles.myIcon} /><p>3</p></button>
-            <button class={styles.myButton} onClick={() => navigate('/user/activity')}><AiOutlineComment class={styles.myIcon}/><p>12</p></button>
-          </div>
+      <div className={styles.logincenterbox}>
+        <p className={styles.unloginmessage}>SSABRY Time에서 자유롭게 소통해보세요</p>
+        <div className={styles.loginContainer}>
+          <button className={styles.userButton}>로그인</button>
+          <button className={styles.userButton}>회원가입</button>
         </div>
       </div>
     </div>
-  )
+  );
+
+  // user정보 있는 상황
+  // return(
+  //   <div className={styles.usercontainer}>
+  //     <div className={styles.usercenterbox}>
+  //       <div className={styles.usertopContainer}>
+  //         <p className={styles.hellomessage}>안녕하세요!</p>
+  //         <button className={styles.mypageButton} onClick={() => navigate('/user')}>마이페이지<IoIosArrowForward size="15"/></button>
+  //       </div>
+
+  //       <div className={styles.usermidContainer}>
+  //         <button class={styles.secondmypageButton} onClick={() => navigate('/user')}>{user}</button><span class={styles.secondhelloMessage}>의 이야기를 들려주세요</span>
+  //         <li class={styles.adverMessage}>진행중인 <button class={styles.adverButton} onClick={() => navigate('/user/advertisement')}>광고</button>가 없습니다.</li>
+  //       </div> 
+        
+  //       <div class={styles.mypageContent}>
+  //         <button class={styles.mymileButton} onClick={() => navigate('/user/advertisement')}><RiDatabase2Line class={styles.mymileIcon} /><p class={styles.mileageTotal}>{mileage}</p></button>
+  //         <div class={styles.mypostsAndcomments}>
+  //           <button class={styles.myButton} onClick={() => navigate('/user/activity')}><BsFileEarmarkText class={styles.myIcon} /><p>3</p></button>
+  //           <button class={styles.myButton} onClick={() => navigate('/user/activity')}><AiOutlineComment class={styles.myIcon}/><p>12</p></button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 } 
