172a2472dc049b161ca97cd2a8b6c135c0ab6e0d
한다솜 <hmdasom@naver.com>
2023-07-21 17:10:32 +0900

    feat: 유저에 따른 게시글 검색 구현

--- server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepositoryImpl.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.Strange505.board.repository;
 
 import com.example.Strange505.board.domain.Article;
 import com.example.Strange505.board.domain.QArticle;
+import com.example.Strange505.user.domain.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +19,25 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom{
         QArticle article = QArticle.article;
         return queryFactory.select(article)
                 .from(article)
-                .where(article.title.eq(title))
+                .where(article.title.contains(title))
+                .fetch();
+    }
+
+    @Override
+    public List<Article> searchByContent(String content) {
+        QArticle article = QArticle.article;
+        return queryFactory.select(article)
+                .from(article)
+                .where(article.content.contains(content))
+                .fetch();
+    }
+
+    @Override
+    public List<Article> searchByUser(Long userId) {
+        QArticle article = QArticle.article;
+        return queryFactory.select(article)
+                .from(article)
+                .where(article.user.id.eq(userId))
                 .fetch();
     }
 }
