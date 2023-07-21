02742f98a01c68daee6c2312deef4e4055e965f8
고동우 <shp05179@gmail.com>
2023-07-21 14:55:18 +0900

    feat: 이메일 인증 api 구현

---
+++ server/Strange505/src/main/java/com/example/Strange505/verificate/Emails.java
@@ -0,0 +1,16 @@
+package com.example.Strange505.verificate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emails {
+    @Id
+    String email;
+}
