9e474e3bf7bc9d41f7e2a8a58231ab9b0f66ae4c
Amyhds <dasomhan524@gmail.com>
2023-07-23 21:17:55 +0900

    feat: 게시판 컨트롤러 구현

--- server/Strange505/src/main/java/com/example/Strange505/board/controller/BoardController.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/controller/BoardController.java
@@ -1,4 +1,31 @@
 package com.example.Strange505.board.controller;
 
+import com.example.Strange505.board.dto.BoardRequestDto;
+import com.example.Strange505.board.dto.BoardResponseDto;
+import com.example.Strange505.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<BoardResponseDto> createBoard(@RequestBody BoardRequestDto boardRequestDto) {
+        BoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto);
+        return new ResponseEntity(boardResponseDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BoardResponseDto> modifyBoard(@PathVariable Long id, @RequestBody BoardRequestDto boardRequestDto) {
+        BoardResponseDto boardResponseDto = boardService.updateBoard(id, boardRequestDto);
+        return new ResponseEntity(boardResponseDto, HttpStatus.OK);
+    }
 }
