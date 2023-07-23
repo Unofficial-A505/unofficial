f5f594935701a404c4b3a071b0525f14ff6bb3f2
Amyhds <dasomhan524@gmail.com>
2023-07-24 01:42:50 +0900

    feat: 게시글 컨트롤러 구현

--- server/Strange505/src/main/java/com/example/Strange505/board/controller/ArticleController.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/controller/ArticleController.java
@@ -14,15 +14,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/article")
+@RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
 
     private final ArticleService articleService;
 
     @PostMapping
-    public void registerArticle(@RequestBody ArticleRequestDto dto, String jwt) {
+    public ResponseEntity<?> registerArticle(@RequestBody ArticleRequestDto dto, String jwt) {
         articleService.createArticle(dto, jwt);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modifyArticle(@PathVariable Long id, @RequestBody ArticleRequestDto dto) {
+        articleService.updateArticle(id, dto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeArticle(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping
@@ -38,12 +51,43 @@ public class ArticleController {
         return new ResponseEntity<>(articleResponseDtoList, HttpStatus.OK);
     }
 
-    @GetMapping("/{title}")
-    public ResponseEntity<List<Article>> getArticlesByTitle() {
+    @GetMapping("/title")
+    public ResponseEntity<List<ArticleResponseDto>> getArticlesByTitle(@RequestParam String title) {
+        List<Article> articles = articleService.getArticlesByTitle(title);
+        List<ArticleResponseDto> articleResponseDtoList = new ArrayList<>();
 
+        articles.stream().forEach(findArticle -> articleResponseDtoList.add(
+                new ArticleResponseDto(findArticle.getTitle(), findArticle.getContent(),
+                        findArticle.getBoard().getName(), findArticle.getNickName(),
+                        findArticle.getCreateTime(), findArticle.getModifyTime())));
+
+        return new ResponseEntity<>(articleResponseDtoList, HttpStatus.OK);
     }
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<>
+    @GetMapping("/content")
+    public ResponseEntity<List<ArticleResponseDto>> getArticlesByContent(@RequestParam String content) {
+        List<Article> articles = articleService.getArticlesByContent(content);
+        List<ArticleResponseDto> articleResponseDtoList = new ArrayList<>();
+
+        articles.stream().forEach(findArticle -> articleResponseDtoList.add(
+                new ArticleResponseDto(findArticle.getTitle(), findArticle.getContent(),
+                        findArticle.getBoard().getName(), findArticle.getNickName(),
+                        findArticle.getCreateTime(), findArticle.getModifyTime())));
+
+        return new ResponseEntity<>(articleResponseDtoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<ArticleResponseDto>> getArticlesByUser(@RequestParam Long userId) {
+        List<Article> articles = articleService.getArticlesByUser(userId);
+        List<ArticleResponseDto> articleResponseDtoList = new ArrayList<>();
+
+        articles.stream().forEach(findArticle -> articleResponseDtoList.add(
+                new ArticleResponseDto(findArticle.getTitle(), findArticle.getContent(),
+                        findArticle.getBoard().getName(), findArticle.getNickName(),
+                        findArticle.getCreateTime(), findArticle.getModifyTime())));
+
+        return new ResponseEntity<>(articleResponseDtoList, HttpStatus.OK);
+    }
 }
