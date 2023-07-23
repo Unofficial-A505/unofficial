41e299dfe6ad4387c42c87ce6085970ee0f1ed05
Amyhds <dasomhan524@gmail.com>
2023-07-23 19:42:03 +0900

    refactor: dto 변경에 따른 test 변경

--- server/Strange505/src/test/java/com/example/Strange505/board/service/BoardServiceImplTest.java
+++ server/Strange505/src/test/java/com/example/Strange505/board/service/BoardServiceImplTest.java
@@ -1,43 +1,38 @@
 package com.example.Strange505.board.service;
 
-import com.example.Strange505.board.domain.Article;
 import com.example.Strange505.board.domain.Board;
-import com.example.Strange505.board.dto.BoardDTO;
+import com.example.Strange505.board.dto.BoardRequestDto;
+import com.example.Strange505.board.dto.BoardResponseDto;
 import com.example.Strange505.board.repository.BoardRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BoardServiceImplTest {
     @Autowired
     private BoardService boardService;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Test
     public void 게시판_생성() {
         String name = "게시판";
-        BoardDTO boardDTO = BoardDTO.builder().name(name).createTime(LocalDateTime.now()).modifyTime(LocalDateTime.now()).build();
-        Board savedBoard = boardService.createBoard(boardDTO);
+        BoardRequestDto dto = BoardRequestDto.builder().name(name).build();
+        BoardResponseDto savedBoard = boardService.createBoard(dto);
 
         org.assertj.core.api.Assertions.assertThat(name).isEqualTo(savedBoard.getName());
     }
 
     @Test
     public void 게시판_수정() {
-        String name = "게시판";
         String modifiedName = "변경";
-        BoardDTO boardDTO = BoardDTO.builder().name(name).createTime(LocalDateTime.now()).modifyTime(LocalDateTime.now()).build();
-        Board savedBoard = boardService.createBoard(boardDTO);
-        BoardDTO modifiedBoardDTO = BoardDTO.builder().name(modifiedName).createTime(LocalDateTime.now()).modifyTime(LocalDateTime.now()).build();
-        boardService.updateBoard(savedBoard.getId(), modifiedBoardDTO);
+        BoardRequestDto dto = BoardRequestDto.builder().name(modifiedName).build();
+        boardService.createBoard(dto);
+        Board board = boardRepository.findById(1L).orElseThrow(() -> new RuntimeException("Board not found"));
+        BoardResponseDto updatedBoard = boardService.updateBoard(board.getId(), dto);
 
-        org.assertj.core.api.Assertions.assertThat(modifiedName).isEqualTo(modifiedBoardDTO.getName());
+        org.assertj.core.api.Assertions.assertThat(modifiedName).isEqualTo(updatedBoard.getName());
 
     }
 
