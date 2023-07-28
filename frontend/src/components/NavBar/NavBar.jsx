32647c57d2c8ee65bdae7a123d5207e345fdfb13
Sseo Jjam <sjh960815@gmail.com>
2023-07-28 20:59:43 +0900

    feat: NavBar 재생성 및 offcanvas 생성

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,49 +1,73 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import styles from "./NavBar.module.css";
 import { useNavigate } from "react-router-dom";
 import main_logo from "./../../assets/images/main_logo.png";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const navigate = useNavigate();
 
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className={`container-fluid ${styles.container}`}>
-        <a className="navbar-brand" style={{ padding: "10px" }} href="/">
-          <img src={main_logo} alt="main_logo" width={150} />
-        </a>
+  const MenuItems = () => (
+    <ul className="navbar-nav">
+      <li className="nav-item">
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className={styles.tab}
+          onClick={() => {
+            navigate("/boards/자유게시판");
+          }}
         >
-          <span className="navbar-toggler-icon"></span>
+          게시판
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <button
-                className={styles.tab}
-                onClick={() => {
-                  navigate("/boards/자유게시판");
-                }}
-              >
-                게시판
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className={styles.tab}>건의하기</button>
-            </li>
-            <li className="nav-item">
-              <button className={styles.tab}>칠판</button>
-            </li>
-          </ul>
+      </li>
+      <li className="nav-item">
+        <button className={styles.tab}>건의하기</button>
+      </li>
+      <li className="nav-item">
+        <button className={styles.tab}>칠판</button>
+      </li>
+    </ul>
+  );
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm d-flex justify-content-center">
+        <div className={`container-fluid ${styles.container}`}>
+          <a className="navbar-brand" style={{ padding: "10px" }} href="/">
+            <img src={main_logo} alt="main_logo" width={150} />
+          </a>
+          <div className="collapse navbar-collapse">
+            <MenuItems />
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+      
+      <div
+        className="offcanvas offcanvas-end"
+        tabIndex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+          <button
+            type="button"
+            className="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <MenuItems />
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
