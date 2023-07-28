f7c0ff8e164ea985b403e0406634a79e8b12eb64
서재현 <sjh960815@naver.com>
2023-07-28 14:47:18 +0900

    fix: 마이페이지 사이드바 새로고침 버그 수정

--- frontend/src/pages/MyPage/MyPage.jsx
+++ frontend/src/pages/MyPage/MyPage.jsx
@@ -1,32 +1,28 @@
 import styles from "./MyPage.module.css";
-import styled from "styled-components";
 
 import TopSpace from "../../components/TopSpace/TopSpace";
 import UnderSpace from "../../components/UnderSpace/UnderSpace";
 
-import { useState } from "react";
-// import { FiSettings } from "@react-icons/all-files/fi/FiSettings";
 import { FiHome } from "@react-icons/all-files/fi/FiHome";
 import { FiActivity } from "@react-icons/all-files/fi/FiActivity";
 import { RiAdvertisementLine } from "@react-icons/all-files/ri/RiAdvertisementLine";
 import { BiLogOut } from "@react-icons/all-files/bi/BiLogOut";
 
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 export default function MyPage() {
-  const [activeTab, setActiveTab] = useState("정보");
-  // const handleTabClick = (tabName) => setActiveTab(tabName);
-  console.log(activeTab);
-  // const NavtopSpace = styled.div`
-  //   width: 100px;
-  //   height: 40px;
-  //   margin: 0 auto;
-  // `;
+  const location = useLocation();
+  const path = location.pathname;
 
-  // const Navhr = styled.hr`
-  //   width: 220px;
-  //   margin: 10px auto;
-  // `;
+  // 현재 경로에서 'activity'나 'advertisement' 문자열이 있는지 확인
+  const isActiveActivity = path.includes("activity");
+  const isActiveAdvertisement = path.includes("advertisement");
+
+  let activeTab = isActiveActivity
+    ? "활동"
+    : isActiveAdvertisement
+    ? "광고"
+    : "정보"; // 기본 탭 설정
 
   return (
     <>
@@ -39,50 +35,41 @@ export default function MyPage() {
             </div>
             <div className={styles.navtopContainer}>
               <div>
-                <div
-                  className={styles.navnameContainer}
-                  onClick={() => setActiveTab("정보")}
-                >
+                <div className={styles.navnameContainer}>
                   <FiHome
                     className={styles.mypagenavIcon}
-                    style={activeTab === "정보" ? { color: "#034BB9 " } : {}}
+                    style={activeTab === "정보" ? { color: "#034BB9 " } : null}
                   />
                   <Link
                     to="/user"
                     className={styles.mypagenavtab}
-                    style={activeTab === "정보" ? { color: "#034BB9 " } : {}}
+                    style={activeTab === "정보" ? { color: "#034BB9 " } : null}
                   >
                     비밀번호 변경
                   </Link>
                 </div>
-                <div
-                  className={styles.navnameContainer}
-                  onClick={() => setActiveTab("활동")}
-                >
+                <div className={styles.navnameContainer}>
                   <FiActivity
                     className={styles.mypagenavIcon}
-                    style={activeTab === "활동" ? { color: "#034BB9" } : {}}
+                    style={activeTab === "활동" ? { color: "#034BB9" } : null}
                   />
                   <Link
                     to="activity"
                     className={styles.mypagenavtab}
-                    style={activeTab === "활동" ? { color: "#034BB9" } : {}}
+                    style={activeTab === "활동" ? { color: "#034BB9" } : null}
                   >
                     내 활동 모아보기
                   </Link>
                 </div>
-                <div
-                  className={styles.navnameContainer}
-                  onClick={() => setActiveTab("광고")}
-                >
+                <div className={styles.navnameContainer}>
                   <RiAdvertisementLine
                     className={styles.mypagenavIcon}
-                    style={activeTab === "광고" ? { color: "#034BB9" } : {}}
+                    style={activeTab === "광고" ? { color: "#034BB9" } : null}
                   />
                   <Link
                     to="advertisement"
                     className={styles.mypagenavtab}
-                    style={activeTab === "광고" ? { color: "#034BB9" } : {}}
+                    style={activeTab === "광고" ? { color: "#034BB9" } : null}
                   >
                     광고 및 마일리지 관리
                   </Link>
