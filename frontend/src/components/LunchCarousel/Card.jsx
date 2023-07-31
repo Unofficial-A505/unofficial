0790bca2b230a241eec3bba02d6e2a4e3d83fe2d
서재현 <sjh960815@naver.com>
2023-07-31 17:26:10 +0900

    fix: 점심캐러셀 사진크기 및 캐러셀 배경색 조정

--- frontend/src/components/LunchCarousel/Card.jsx
+++ frontend/src/components/LunchCarousel/Card.jsx
@@ -1,5 +1,5 @@
 import styles from "./Card.module.css";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
