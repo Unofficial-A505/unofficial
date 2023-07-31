95cfecf899c4459b5e426050e7604803300937c2
paengjiwoo <aprilzu0427@gmail.com>
2023-07-31 17:53:13 +0900

    feat: 마이페이지 내활동내역 UI 1차 수정

--- frontend/src/components/BoardView/BoardView.jsx
+++ frontend/src/components/BoardView/BoardView.jsx
@@ -2,6 +2,7 @@ import styles from './BoardView.module.css'
 import axios from 'axios';
 import { useEffect, useState, useParams } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import PostView from '../PostView/PostView'
 
@@ -11,19 +12,20 @@ export default function BoardView( ){
   const pageSize = 10;
 
   const navigate = useNavigate();
+  const URL = useSelector(state => state.URL.API_URL)
 
   useEffect(() => {
 
     axios({
       method: "get",
-      url: `http://127.0.0.1:8000/api/v1/articles`,
+      url: `${URL}api/v1/articles`,
       // headers: {
       //   Authorization: `Token ${this.$store.state.token}`,
       // }
       })
       .then((res) => {
         console.log(res.data);
-        setPosts(res.data.slice(0, 15));
+        setPosts(res.data.slice(0, 10));
       })
       .catch((err) => console.log(err))
     return () => {  
--- frontend/src/pages/BoardsAll/BoardsView/BoardsView.jsx
+++ frontend/src/pages/BoardsAll/BoardsView/BoardsView.jsx
@@ -2,6 +2,7 @@ import styles from './BoardsView.module.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import PostsView from '../../../components/PostView/PostView'
 
@@ -12,12 +13,13 @@ export default function BoardsView( ){
     boardTitle = '자유게시판'
   }
   const navigate = useNavigate();
+  const URL = useSelector(state => state.URL.API_URL)
 
   useEffect(() => {
 
     axios({
       method: "get",
-      url: `http://127.0.0.1:8000/api/v1/articles`,
+      url: `${URL}api/v1/articles`,
       // headers: {
       //   Authorization: `Token ${this.$store.state.token}`,
       // }
