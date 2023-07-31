0448b021a6d786324a22a94ce7e5a4ad55107a73
한다솜 <hmdasom@naver.com>
2023-07-31 10:42:09 +0900

    refactor: 게시글 조회 기능 동적 쿼리로 리팩토링

--- server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepositoryImpl.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/repository/ArticleRepositoryImpl.java
@@ -2,8 +2,10 @@ package com.example.Strange505.board.repository;
 
 
 import com.example.Strange505.board.domain.Article;
-import com.example.Strange505.board.domain.QArticle;
+import static com.example.Strange505.board.domain.QArticle.article;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -14,41 +16,36 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Article> searchByTitle(String title, Long boardId) {
-        QArticle article = QArticle.article;
-        if (boardId == 0) {
-            return queryFactory.select(article)
-                    .from(article)
-                    .where(article.title.contains(title))
-                    .fetch();
-        } else {
-            return queryFactory.select(article)
-                    .from(article)
-                    .where(article.title.contains(title), article.board.id.eq(boardId))
-                    .fetch();
-        }
-
+    public List<Article> searchByTitleAndContent(String title, String content, Long boardId) {
+        return queryFactory
+                .selectFrom(article)
+                .where(eqBoard(boardId).and(titleLike(title).or(contentLike(content))))
+                .fetch();
     }
 
-    @Override
-    public List<Article> searchByContent(String content, Long boardId) {
-        QArticle article = QArticle.article;
-        if (boardId == 0) {
-            return queryFactory.select(article)
-                    .from(article)
-                    .where(article.content.contains(content))
-                    .fetch();
-        } else {
-            return queryFactory.select(article)
-                    .from(article)
-                    .where(article.content.contains(content), article.board.id.eq(boardId))
-                    .fetch();
+    private BooleanExpression titleLike(String title) {
+        if(StringUtils.isEmpty(title)){
+            return null;
         }
+        return article.title.contains(title);
+    }
+
+    private BooleanExpression contentLike(String content) {
+        if (StringUtils.isEmpty(content)) {
+            return null;
+        }
+        return article.content.contains(content);
+    }
+
+    private BooleanExpression eqBoard(Long boardId) {
+        if (boardId == null) {
+            return null;
+        }
+        return article.board.id.eq(boardId);
     }
 
     @Override
     public List<Article> searchByUser(Long userId) {
-        QArticle article = QArticle.article;
         return queryFactory.select(article)
                 .from(article)
                 .where(article.user.id.eq(userId))
@@ -57,7 +54,6 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
 
     @Override
     public List<Article> searchByBoard(Long boardId) {
-        QArticle article = QArticle.article;
         return queryFactory.select(article)
                 .from(article)
                 .where(article.board.id.eq(boardId))
