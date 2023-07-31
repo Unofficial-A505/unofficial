51576302f7254dd8c2947a71eb647a330c4935b3
서재현 <sjh960815@naver.com>
2023-07-31 17:27:17 +0900

    필요없는 CSS 제거

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer(){
+  return(
+    <div className={styles.undercontainer}>
+      Footer
+    </div>
+  );
+}
--- frontend/src/components/UserinfoBox/UserinfoBox.jsx
+++ frontend/src/components/UserinfoBox/UserinfoBox.jsx
@@ -18,42 +18,42 @@ export default function UserinfoBox(){
   const mileage = 5100
 
   const navigate = useNavigate();
-  // // user정보 없는 상황
-  // return(
-  //   <div className={styles.usercontainer}>
-  //     <p className={styles.unloginmessage}>
-  //       언오피셜을 더 편리하게 이용하세요
-  //     </p>
-  //     <button className={styles.loginButton} onClick={()=>{setModalOpen(true)}}>로그인</button>
-  //     {modalOpen && <Login setModalOpen={setModalOpen} />}
-  //     <div className={styles.signupBox}>
-  //       <p onClick={()=>{navigate('/')}}>비밀번호 찾기</p>
-  //       <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
-  //       <p onClick={()=>{navigate('/signup')}}>회원가입</p>
-  //     </div>
-  //   </div>
-  // );
-
-  // user정보 있는 상황
+  // user정보 없는 상황
   return(
     <div className={styles.usercontainer}>
-      <div className={styles.usertopContainer}>
-        <p className={styles.hellomessage}>안녕하세요!</p>
-        <p className={styles.mypage} style={{color:'#034BB9'}} onClick={() => navigate('/user/password')}>마이페이지<IoIosArrowForward /></p>
-      </div>
-
-      <div className={styles.usermidContainer}>
-        <span class={styles.secondmypage} onClick={() => navigate('/user/password')}>{user}</span><span class={styles.secondhelloMessage}>의 이야기를 들려주세요</span>
-        <li class={styles.adverMessage}>진행중인 <span class={styles.adverButton} onClick={() => navigate('/user/advertisement/myadv')}>광고</span>가 없습니다.</li>
-      </div> 
-      
-      <div class={styles.mypageContent}>
-        <p class={styles.mymileButton} onClick={() => navigate('/user/advertisement/mymile')}><RiDatabase2Line class={styles.mymileIcon} /><p class={styles.mileageTotal}>{mileage}</p></p>
-        <div class={styles.mypostsAndcomments}>
-          <p class={styles.myButton} onClick={() => navigate('/user/activity/myposts')}><BsFileEarmarkText class={styles.myIcon} /><p>3</p></p>
-          <p class={styles.myButton} onClick={() => navigate('/user/activity/mycomments')}><AiOutlineComment class={styles.myIcon}/><p>12</p></p>
-        </div>
+      <p className={styles.unloginmessage}>
+        언오피셜을 더 편리하게 이용하세요
+      </p>
+      <button className={styles.loginButton} onClick={()=>{setModalOpen(true)}}>로그인</button>
+      {modalOpen && <Login setModalOpen={setModalOpen} />}
+      <div className={styles.signupBox}>
+        <p onClick={()=>{navigate('/')}}>비밀번호 찾기</p>
+        <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+        <p onClick={()=>{navigate('/signup')}}>회원가입</p>
       </div>
     </div>
-  )
+  );
+
+  // // user정보 있는 상황
+  // return(
+  //   <div className={styles.usercontainer}>
+  //     <div className={styles.usertopContainer}>
+  //       <p className={styles.hellomessage}>안녕하세요!</p>
+  //       <p className={styles.mypage} style={{color:'#034BB9'}} onClick={() => navigate('/user/password')}>마이페이지<IoIosArrowForward /></p>
+  //     </div>
+
+  //     <div className={styles.usermidContainer}>
+  //       <span class={styles.secondmypage} onClick={() => navigate('/user/password')}>{user}</span><span class={styles.secondhelloMessage}>의 이야기를 들려주세요</span>
+  //       <li class={styles.adverMessage}>진행중인 <span class={styles.adverButton} onClick={() => navigate('/user/advertisement/myadv')}>광고</span>가 없습니다.</li>
+  //     </div> 
+      
+  //     <div class={styles.mypageContent}>
+  //       <p class={styles.mymileButton} onClick={() => navigate('/user/advertisement/mymile')}><RiDatabase2Line class={styles.mymileIcon} /><p class={styles.mileageTotal}>{mileage}</p></p>
+  //       <div class={styles.mypostsAndcomments}>
+  //         <p class={styles.myButton} onClick={() => navigate('/user/activity/myposts')}><BsFileEarmarkText class={styles.myIcon} /><p>3</p></p>
+  //         <p class={styles.myButton} onClick={() => navigate('/user/activity/mycomments')}><AiOutlineComment class={styles.myIcon}/><p>12</p></p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
   } 
