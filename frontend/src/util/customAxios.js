48bd2a75b9b26c6cc7fe26a7353f435ce1bcccdd
고동우 <shp05179@gmail.com>
2023-08-01 17:21:12 +0900

    fix: 요청 url 변경
    localhost -> unofficial.kr

--- frontend/src/util/customAxios.js
+++ frontend/src/util/customAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store/store';
 
 const customAxios = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://unofficial.kr',
 });
 
 // Add a request interceptor
