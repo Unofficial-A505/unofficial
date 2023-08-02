e83db2b51c3ff2571bcfe326d0d8731f5b58eea5
정승구 <jsggo2001@naver.com>
2023-08-02 13:28:40 +0900

    feat: property serve dev 분리

--- server/Strange505/src/test/java/com/example/Strange505/Strange505ApplicationTests.java
+++ server/Strange505/src/test/java/com/example/Strange505/Strange505ApplicationTests.java
@@ -1,14 +1,14 @@
-package com.example.Strange505;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Strange505ApplicationTests {
-
-	@Test
-	void contextLoads() {
-		
-	}
-
-}
+//package com.example.Strange505;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Strange505ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
