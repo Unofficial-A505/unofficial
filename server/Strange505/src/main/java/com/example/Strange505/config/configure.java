a86f2e4e3167a7e3c8b316b0881915c411a253e7
정승구 <jsggo2001@naver.com>
2023-08-03 17:28:57 +0900

    fix: cors config origin add

--- server/Strange505/src/main/java/com/example/Strange505/config/configure.java
+++ server/Strange505/src/main/java/com/example/Strange505/config/configure.java
@@ -12,7 +12,7 @@ public class configure implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("https://unofficial.kr")
+                    .allowedOrigins("https://unofficial.kr","https://dev.unofficial.kr")
                     .allowedMethods("PUT", "DELETE","POST","GET")
                     .allowCredentials(false).maxAge(3600);
         }
