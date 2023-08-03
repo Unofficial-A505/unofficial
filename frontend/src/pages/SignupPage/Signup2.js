d189a5e2ff2c833cc7ce3cf810152730517930d7
서재현 <sjh960815@naver.com>
2023-08-03 13:25:38 +0900

    fix: 아이폰 강력한 비밀번호로도 회원가입 가능하게 수정

--- frontend/src/pages/SignupPage/Signup2.js
+++ frontend/src/pages/SignupPage/Signup2.js
@@ -87,25 +87,22 @@ export default function Signup2() {
   }
   // 비밀번호 입력 오류 확인
   const checkPassword = () => {
-    let pw = userPassword1
+    let pw = userPassword2
     let num = pw.search(/[0-9]/g);
     let eng = pw.search(/[a-z]/ig);
-    let spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+    let spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?-]/gi);
 
-    if (pw.length < 8 || pw.length > 20) {
-      alert("8 ~ 20자리 이내로 입력해주세요.");
-      return false
-    }
-    if (pw.search(/\s/) !== -1) {
-      alert("비밀번호는 공백 없이 입력해주세요.");
-      return false
-    }
-    if (num < 0 || eng < 0 || spe < 0) {
-      alert("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
-      return false
-    }
     if (passwordMismatch) {
       alert('비밀번호가 일치하지 않습니다.')
+      return false
+    } else if (pw.length < 8 || pw.length > 20) {
+      alert("8 ~ 20자리 이내로 입력해주세요.");
+      return false
+    } else if (pw.search(/\s/) !== -1) {
+      alert("비밀번호는 공백 없이 입력해주세요.");
+      return false
+    } else if (num === -1 || eng === -1 || spe === -1) {
+      alert("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
       return false
     }
     return true
