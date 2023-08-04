c6910b8ed5ecbdf26b5b9812689706dc9963e82d
서재현 <sjh960815@naver.com>
2023-08-04 10:45:30 +0900

    fix: 마이페이지 사이드 navbar 코드 정리

--- frontend/src/pages/MyPage/MyPage.jsx
+++ frontend/src/pages/MyPage/MyPage.jsx
@@ -5,9 +5,9 @@ import TopSpace from "../../components/TopSpace/TopSpace";
 import { FiHome } from "@react-icons/all-files/fi/FiHome";
 import { FiActivity } from "@react-icons/all-files/fi/FiActivity";
 import { RiAdvertisementLine } from "@react-icons/all-files/ri/RiAdvertisementLine";
-import { BiLogOut } from "@react-icons/all-files/bi/BiLogOut";
+import { RiLogoutCircleLine } from "@react-icons/all-files/ri/RiLogoutCircleLine";
 
-import { Outlet, Link, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import customAxios from "../../util/customAxios";
 import { useSelector } from "react-redux";
 
@@ -27,7 +27,7 @@ export default function MyPage() {
   const path = location.pathname;
   const isActiveActivity = path.includes("activity");
   const isActiveAdvertisement = path.includes("advertisement");
-  const isManagement = path.includes("management");  //관리자
+  const isManagement = path.includes("management"); //관리자
   const [role, setRole] = useState(null);
 
   useEffect(() => {
@@ -58,71 +58,65 @@ export default function MyPage() {
       <div className={styles.mypageContainer}>
         <div className={styles.mypageNavContainer}>
           <nav>
-            <div className={styles.navtitleContainer}>
+            <div className={styles.navTitleContainer}>
               <h4>마이페이지</h4>
             </div>
-            <div className={styles.navtopContainer}>
-              <div>
-                <div className={styles.navnameContainer}>
-                  <FiHome
-                    className={styles.mypagenavIcon}
-                    style={activeTab === "정보" ? { color: "#034BB9 " } : null}
-                  />
-                  <Link
-                    to="/user/password"
-                    className={styles.mypagenavtab}
-                    style={activeTab === "정보" ? { color: "#034BB9 " } : null}
-                  >
-                    내 정보 수정
-                  </Link>
-                </div>
-                <div className={styles.navnameContainer}>
-                  <FiActivity
-                    className={styles.mypagenavIcon}
-                    style={activeTab === "활동" ? { color: "#034BB9" } : null}
-                  />
-                  <Link
-                    to="activity/myposts"
-                    className={styles.mypagenavtab}
-                    style={activeTab === "활동" ? { color: "#034BB9" } : null}
-                  >
-                    내 활동 모아보기
-                  </Link>
-                </div>
-                <div className={styles.navnameContainer}>
-                  <RiAdvertisementLine
-                    className={styles.mypagenavIcon}
-                    style={activeTab === "광고" ? { color: "#034BB9" } : null}
-                  />
-                  <Link
-                    to="advertisement/mymile"
-                    className={styles.mypagenavtab}
-                    style={activeTab === "광고" ? { color: "#034BB9" } : null}
-                  >
-                    광고 및 마일리지 관리
-                  </Link>
-                </div>
-                {role === 'ADMIN' && (
-                  <div className={styles.navnameContainer}>
-                    <RiAdvertisementLine
-                      className={styles.mypagenavIcon}
-                      style={activeTab === "관리자" ? { color: "#034BB9" } : null}
-                    />
-                    <Link
-                      to="management"
-                      className={styles.mypagenavtab}
-                      style={activeTab === "관리자" ? { color: "#034BB9" } : null}
-                    >
-                      광고 승인 및 취소(관리자)
-                    </Link>
-                  </div>
-                )}
+            <div>
+              <div
+                className={styles.navTab}
+                onClick={() => navigate("password")}
+                style={
+                  activeTab === "정보"
+                    ? { color: "#034BB9" }
+                    : { color: "#666a71" }
+                }
+              >
+                <FiHome className="me-2" size={20} />
+                <p>내 정보 수정</p>
               </div>
+              <div
+                className={styles.navTab}
+                onClick={() => navigate("activity/myposts")}
+                style={
+                  activeTab === "활동"
+                    ? { color: "#034BB9" }
+                    : { color: "#666a71" }
+                }
+              >
+                <FiActivity className="me-2" size={20} />
+                <p>내 활동 모아보기</p>
+              </div>
+              <div
+                className={styles.navTab}
+                onClick={() => navigate("advertisement/mymile")}
+                style={
+                  activeTab === "광고"
+                    ? { color: "#034BB9" }
+                    : { color: "#666a71" }
+                }
+              >
+                <RiAdvertisementLine className="me-2" size={20} />
+                <p>광고 및 마일리지 관리</p>
+              </div>
+              {role === "ADMIN" && (
+                <div
+                  className={styles.navTab}
+                  onClick={() => navigate("management")}
+                  style={
+                    activeTab === "관리자"
+                      ? { color: "#034BB9" }
+                      : { color: "#666a71" }
+                  }
+                >
+                  <RiAdvertisementLine className="me-2" size={20} />
+                  <p>광고 승인 및 취소(관리자)</p>
+                </div>
+              )}
             </div>
           </nav>
-          <div className={styles.navlogoutContainer}>
-            <BiLogOut />
-            <button className={styles.logoutButton}>Logout</button>
+          <div className={styles.logoutTab}>
+            <RiLogoutCircleLine className="me-2" size={20} />
+            <p>Logout</p>
           </div>
         </div>
         <div className={styles.mypageContentContainer}>
