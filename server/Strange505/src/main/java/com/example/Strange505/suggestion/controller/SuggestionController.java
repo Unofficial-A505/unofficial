16c5f9e45585a6858c48a5206bb0274c1178b4e9
kanghee96 <kanghee96@gmail.com>
2023-08-04 14:46:13 +0900

    hotfix: cors 수정

--- server/Strange505/src/main/java/com/example/Strange505/suggestion/controller/SuggestionController.java
+++ server/Strange505/src/main/java/com/example/Strange505/suggestion/controller/SuggestionController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/suggestions")
-@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class SuggestionController {
     @Autowired
