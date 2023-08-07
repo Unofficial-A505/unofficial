fcbac5bb4f9aef96235475b873c3ac806b72290f
서재현 <sjh960815@naver.com>
2023-08-07 16:21:34 +0900

    fix: 점심식단 카드 1개 이하면 nav버튼 숨김 & 게시글 페이지 탭제목 수정

--- frontend/src/pages/PostDetail/PostDetail.jsx
+++ frontend/src/pages/PostDetail/PostDetail.jsx
@@ -26,43 +26,48 @@ import { IoChatboxOutline } from "@react-icons/all-files/io5/IoChatboxOutline";
 // 조회수 아이콘
 import { AiOutlineEye } from "@react-icons/all-files/ai/AiOutlineEye";
 
-import { boardArticlesAll } from '../../api/boards'
-import { postDetailApi, postDeleteApi, postRecommendInputApi } from '../../api/posts'
-import { postCommentsApi, postCommentCreateApi, postCommentUpdateApi, postCommentDeleteApi } from '../../api/comments'
+import { boardArticlesAll } from "../../api/boards";
+import {
+  postDetailApi,
+  postDeleteApi,
+  postRecommendInputApi,
+} from "../../api/posts";
+import {
+  postCommentsApi,
+  postCommentCreateApi,
+  postCommentUpdateApi,
+  postCommentDeleteApi,
+} from "../../api/comments";
 
-import customAxios from '../../util/customAxios'
+import customAxios from "../../util/customAxios";
 import useDocumentTitle from "../../useDocumentTitle";
 
 // API import
 export default function PostDetail() {
   const navigate = useNavigate();
-  // const { boardTitleFromUrl } = useParams();
+  const setDocumentTitle = useDocumentTitle();
+
   const { boardId } = useParams();
   const { postId } = useParams();
-  const [ boardTitle, setBoardTitle ] = useState('')
-  const [ postDetail, setpostDetail ] = useState({})
-  const [ createcomment, setcreateComment] = useState("");
-  const [ comments, setComments ] = useState([])
-  const [ commentsInfo, setCommentsInfo ] = useState({})
-  const [ commentnickName, setcommentnickName] = useState("")
-  const [ articleList, setarticleList ] = useState([]);
-  const [ recommendedState, setrecommendedState ] = useState(false)
+  const [boardTitle, setBoardTitle] = useState("");
+  const [postDetail, setpostDetail] = useState({});
+  const [createcomment, setcreateComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [commentsInfo, setCommentsInfo] = useState({});
+  const [commentnickName, setcommentnickName] = useState("");
+  const [articleList, setarticleList] = useState([]);
+  const [recommendedState, setrecommendedState] = useState(false);
   const commentElement = useRef(null);
-
-  useDocumentTitle({boardTitle});
 
   // 댓글 가져오기
   const getComment = () => {
     customAxios({
       method: "get",
       url: `${process.env.REACT_APP_SERVER}/api/comments/article/${postId}`,
-      // headers: {
-      //   Authorization: `Token ${this.$store.state.token}`,
-      // }
     })
       .then((res) => {
         setComments(res.data.content);
-        setCommentsInfo(res.data)
+        setCommentsInfo(res.data);
       })
       .catch((err) => console.log(err));
   };
@@ -71,64 +76,66 @@ export default function PostDetail() {
     customAxios({
       method: "get",
       url: `${process.env.REACT_APP_SERVER}/api/articles/${postId}`,
-      // headers: {
-      //   Authorization: `Token ${this.$store.state.token}`,
-      // }
     })
       .then((res) => {
-        console.log('detail', res.data);
-        setpostDetail(res.data)
-        // setTitle(res.data.title);
-        // setContent(res.data.content);
-        setBoardTitle(res.data.boardName)
+        console.log("detail", res.data);
+        setpostDetail(res.data);
+        setBoardTitle(res.data.boardName);
+        setDocumentTitle(res.data.boardName);
       })
       .catch((err) => console.log(err));
 
-      window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: "smooth" });
 
     // 게시글 상세정보 가져오기
     postDetailApi(postId)
-    .then((res) => setpostDetail(res))
-    .catch((err) => console.log(err));
+      .then((res) => setpostDetail(res))
+      .catch((err) => console.log(err));
 
     window.scrollTo({ top: 0, behavior: "smooth" });
     getComment();
 
-    return () => {  
-      console.log('unmounted')}
-    }, [postId]);
-    
+    return () => {
+      console.log("unmounted");
+    };
+  }, [postId]);
+
   // 게시글 삭제
   const postDelete = () => {
     postDeleteApi(postId)
-    .then(() => {navigate(`/boards/${boardId}`)})
-    .catch((err) => console.log(err));
-    };
-  
+      .then(() => {
+        navigate(`/boards/${boardId}`);
+      })
+      .catch((err) => console.log(err));
+  };
+
   // 게시글 추천
   const postRecommendedInput = () => {
     const articleId = postId;
     postRecommendInputApi(articleId)
-    .then(() => setrecommendedState((prev) => !prev))
-    .catch((res) => console.log(res))
-  }
-  
+      .then(() => setrecommendedState((prev) => !prev))
+      .catch((res) => console.log(res));
+  };
+
   // 댓글 생성
   const commentCreate = () => {
     let text = document.querySelector(".textarea").value;
     text = text.replaceAll(/(\n|\r\n)/g, "<br>");
 
-    if (!commentnickName) { alert('댓글 닉네임을 입력해주세요!')
-    } else if (!createcomment) { alert('댓글을 입력해주세요!')
+    if (!commentnickName) {
+      alert("댓글 닉네임을 입력해주세요!");
+    } else if (!createcomment) {
+      alert("댓글을 입력해주세요!");
     } else {
-      const content = createcomment
+      const content = createcomment;
       const parentId = 0;
       const articleId = postId;
-      const nickName = commentnickName
+      const nickName = commentnickName;
 
-    postCommentCreateApi(articleId, content, parentId, nickName)
-    .then(() => getComment())
-    .catch((err) => console.log(err));} 
+      postCommentCreateApi(articleId, content, parentId, nickName)
+        .then(() => getComment())
+        .catch((err) => console.log(err));
+    }
   };
 
   // 댓글 수정
@@ -136,28 +143,28 @@ export default function PostDetail() {
     let text = document.querySelector(".textarea").value;
     text = text.replaceAll(/(\n|\r\n)/g, "<br>");
 
-    const content = updateComment
+    const content = updateComment;
     const parentId = 0;
     const articleId = postId;
     postCommentUpdateApi(id, articleId, content, parentId)
-    .then(() => getComment())
-    .catch((err) => console.log(err))
-    }
+      .then(() => getComment())
+      .catch((err) => console.log(err));
+  };
 
   // 댓글 삭제
   const CommentDelete = (id) => {
     postCommentDeleteApi(id)
-    .then(() => getComment())
-    .catch((err) => console.log(err));
+      .then(() => getComment())
+      .catch((err) => console.log(err));
   };
 
   const username = "9기 서울";
   const timeago = "21분 전";
 
-  const createTime = postDetail.createTime
-  const updateTime = postDetail.modifyTime
-  const createTime_modify = createTime?.slice(0, 10)
-  const updateTime_modify = updateTime?.slice(0, 10)
+  const createTime = postDetail.createTime;
+  const updateTime = postDetail.modifyTime;
+  const createTime_modify = createTime?.slice(0, 10);
+  const updateTime_modify = updateTime?.slice(0, 10);
   // console.log(createTime_modify, updateTime_modify)
 
   return (
@@ -169,7 +176,9 @@ export default function PostDetail() {
             <span className={styles.boardTitle}>{postDetail.boardName}</span>
             <button
               className={styles.grayoutbutton}
-              onClick={() => navigate(`/boards/${boardTitle}`, { state : postDetail.boardId })}
+              onClick={() =>
+                navigate(`/boards/${boardTitle}`, { state: postDetail.boardId })
+              }
             >
               <IoIosArrowBack />
               목록으로 돌아가기
@@ -178,12 +187,17 @@ export default function PostDetail() {
           <div className={styles.postContainer}>
             <div>
               <div className={styles.postTitle}>{postDetail.title}</div>
-              <div className={styles.postusername}>{postDetail.nickName === null || postDetail.nickName===""? "익명" : postDetail.nickName}</div>
+              <div className={styles.postusername}>
+                {postDetail.nickName === null || postDetail.nickName === ""
+                  ? "익명"
+                  : postDetail.nickName}
+              </div>
               <div className={styles.dateViews}>
                 <div className={styles.posttimeago}>
                   <IoRocketOutline className={styles.tabIcon} size="20" />
-                  {createTime_modify} 
-                  {createTime_modify !== updateTime_modify && ` (수정 : ${updateTime_modify})`}
+                  {createTime_modify}
+                  {createTime_modify !== updateTime_modify &&
+                    ` (수정 : ${updateTime_modify})`}
                 </div>
                 <div className={styles.posttimeago}>
                   <AiOutlineEye className={styles.tabIcon} size="19" />
@@ -203,11 +217,21 @@ export default function PostDetail() {
                 {postDetail.likes}
               </div>
               <div className={styles.postupdateBottom}>
-                <div onClick={() => navigate(`/boards/${boardId}/${postId}/update`, { state : postDetail })} className={styles.postupdateBottomtab}>
+                <div
+                  onClick={() =>
+                    navigate(`/boards/${boardId}/${postId}/update`, {
+                      state: postDetail,
+                    })
+                  }
+                  className={styles.postupdateBottomtab}
+                >
                   <HiOutlinePencilAlt size="15" />
                   update
                 </div>
-                <div onClick={postDelete} className={styles.postupdateBottomtab}>
+                <div
+                  onClick={postDelete}
+                  className={styles.postupdateBottomtab}
+                >
                   <IoTrashOutline size="15" />
                   delete
                 </div>
@@ -224,7 +248,15 @@ export default function PostDetail() {
 
             <div className={styles.commentnickName}>
               <div>닉네임</div>
-              <input className={styles.commentnickNameInput} type="text" placeholder="닉네임을 입력하세요" onChange={(e) => {setcommentnickName(e.target.value); console.log(commentnickName);}}/>
+              <input
+                className={styles.commentnickNameInput}
+                type="text"
+                placeholder="닉네임을 입력하세요"
+                onChange={(e) => {
+                  setcommentnickName(e.target.value);
+                  console.log(commentnickName);
+                }}
+              />
             </div>
             <div className={styles.commentbox}>
               <textarea
@@ -232,8 +264,8 @@ export default function PostDetail() {
                 type="text"
                 onChange={(e) => {
                   setcreateComment(e.target.value);
-                  console.log(createcomment)
-                 }}
+                  console.log(createcomment);
+                }}
                 placeholder="댓글을 작성해보세요"
               />
               <button className={styles.commentButton} onClick={commentCreate}>
@@ -244,11 +276,16 @@ export default function PostDetail() {
 
           <div className={styles.postContainer}>
             <hr />
-            {comments.map((comment, index) =>
-              <div key={index}> 
-                <CommentView comment={comment} CommentDelete={CommentDelete} commentUpdate={commentUpdate} postId={postId}/>
+            {comments.map((comment, index) => (
+              <div key={index}>
+                <CommentView
+                  comment={comment}
+                  CommentDelete={CommentDelete}
+                  commentUpdate={commentUpdate}
+                  postId={postId}
+                />
               </div>
-            )}
+            ))}
           </div>
           <div>
             <nav className={styles.commentPagination} aria-label="...">
@@ -288,24 +325,30 @@ export default function PostDetail() {
           </div> */}
 
           <br />
-          
+
           <div className={styles.moreTopbar}>
             <button
               className={styles.buttonlayoutDel}
-              onClick={() => navigate(`/boards/${boardTitle}`, { state : postDetail.boardId })}
+              onClick={() =>
+                navigate(`/boards/${boardTitle}`, { state: postDetail.boardId })
+              }
             >
-              <span className={styles.boardmoreTitleA}>{postDetail.boardName}</span>
+              <span className={styles.boardmoreTitleA}>
+                {postDetail.boardName}
+              </span>
               <span className={styles.boardmoreTitleB}>글 더 보기</span>
             </button>
             <button
               className={styles.grayoutbutton}
-              onClick={() => navigate(`/boards/${boardTitle}`, { state : postDetail.boardId })}
+              onClick={() =>
+                navigate(`/boards/${boardTitle}`, { state: postDetail.boardId })
+              }
             >
               목록 보기
               <IoIosArrowForward />
             </button>
           </div>
-          <BoardView posts={articleList}/>
+          <BoardView posts={articleList} />
         </span>
 
         <span className={styles.sideviewContainer}>
