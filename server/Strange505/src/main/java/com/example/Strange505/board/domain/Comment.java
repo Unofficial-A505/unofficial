6e98a6286f91452119b283cc2ebc9871097656cf
한다솜 <hmdasom@naver.com>
2023-08-10 15:39:06 +0900

    chore: 불필요한 import 삭제

--- server/Strange505/src/main/java/com/example/Strange505/board/domain/Comment.java
+++ server/Strange505/src/main/java/com/example/Strange505/board/domain/Comment.java
@@ -2,8 +2,10 @@ package com.example.Strange505.board.domain;
 
 import com.example.Strange505.user.domain.User;
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.security.core.parameters.P;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
