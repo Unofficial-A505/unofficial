a976498f7ed8149428a2e15d6a4ee3cf1075f9a4
서재현 <sjh960815@naver.com>
2023-08-14 13:02:18 +0900

    fix: footer 전화번호 삭제

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -25,7 +25,6 @@ export default function Footer() {
             <p>개발팀: 이상한 개발팀 오영오</p>
             <p>개발자 대표: 정승구</p>
             <p>이메일: jsgoo2001@naver.com</p>
-            <p>전화번호: 010-1234-5678</p>
           </div>
         </div>
 
