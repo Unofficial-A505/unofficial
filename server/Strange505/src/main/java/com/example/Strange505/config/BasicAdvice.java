b44b9c229323a70b512110b28688143b6f9c78fb
정승구 <jsggo2001@naver.com>
2023-08-16 16:10:21 +0900

    feat: runtime handler add

---
+++ server/Strange505/src/main/java/com/example/Strange505/config/BasicAdvice.java
@@ -0,0 +1,18 @@
+package com.example.Strange505.config;
+
+import com.example.Strange505.vo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.example.Strange505") // 외에 여러 지정 방법이 있다.
+public class BasicAdvice {
+    @ExceptionHandler
+    public ResponseEntity<Result> RuntimeExHandler(RuntimeException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ResponseEntity<>(Result.fail(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
