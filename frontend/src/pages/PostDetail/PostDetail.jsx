08eac24af312f30ebca3f34e1eb1865ca571a01f
paengjiwoo <aprilzu0427@gmail.com>
2023-08-16 11:07:18 +0900

    fix: pagination 스타일 수정

--- frontend/src/pages/PostDetail/PostDetail.jsx
+++ frontend/src/pages/PostDetail/PostDetail.jsx
@@ -84,7 +84,7 @@ export default function PostDetail() {
       method: "get",
       url: `/api/comments/article/${postId}?page=${
         currcommentPage - 1
-      }&size=${20}`,
+      }&size=${2}`,
     })
       .then((res) => {
         setComments(res.data.content);
